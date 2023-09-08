f133cb0fe2033c193ffa8c5652070d21a1a5ce24
Benny20s <ben.sepulvedat@duocuc.cl>
2023-09-08 01:00:05 -0300

    jua

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
@@ -13,12 +13,9 @@ const routes: Routes = [
   },
   {
     path: 'sesion',
-    loadChildren: () => import('./sesion/sesion.module').then( m => m.SesionPageModule)
-  },
-  {
-    path: 'sesion',
     loadChildren: () => import('./pages/sesion/sesion.module').then( m => m.SesionPageModule)
   },
+  
 ];
 
 @NgModule({
